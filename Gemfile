cc28ee5efb5f0c955c7f48d8f1f40a7874f07a5d
Alex Cummaudo <alexcu@me.com>
2016-08-01 16:06:04 +1000

    CONFIG: Upgrade to Middleman v4

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@
 # the following line to use 'http://' instead
 source 'https://rubygems.org'
 
-gem 'middleman', '~>3.4.0'
+gem 'middleman', '~>4.1.0'
 
 # Live-reloading plugin
-gem 'middleman-livereload', '~> 3.1.0'
+gem 'middleman-livereload', '~>3.4.0'
 
 # For faster file watcher updates on Windows:
 gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
@@ -17,7 +17,11 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'require_all', '1.3.3'
 
 # Autoprefixer for all vendor-specific CSS attributes
-gem 'middleman-autoprefixer', '2.6.1'
+gem 'middleman-autoprefixer', '~>2.7.0'
+
+# Use sprokets for assets + compass
+gem 'middleman-sprockets', '4.0.0'
+gem 'middleman-compass', '4.0.1'
 
 # Boostrap as SASS (require false is important)
 gem 'bootstrap-sass', '3.3.6', require: false
@@ -27,14 +31,13 @@ gem 'jquery-middleman', '3.1.2'
 
 # Markdown redcarpet parsing engine + syntax highlighter for middleman
 gem 'redcarpet', '3.3.3'
+gem 'middleman-syntax', '~>3.0.0'
 
 # Publish middleman website to gh-pages
-gem 'middleman-gh-pages', '0.0.3'
+gem 'middleman-gh-pages', '~>0.3.0'
 
 # Rake tasks
 gem 'rake', '10.5.0'
 
 # Ruby slim for templating engine
 gem 'slim', '3.0.6'
-
-gem 'middleman-rouge', '0.0.1'
--- config.rb
+++ config.rb
@@ -11,31 +11,12 @@
 # Page options, layouts, aliases and proxies
 ###
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
 # Root Layout
-with_layout :root do
-  page "/*"
-end
+page '/*', layout: :root
 # Article layout
-with_layout :articles do
-  page "/articles/*"
-end
+page '/articles/*', layout: :articles
 # API layout
-with_layout :api do
-  page "/api/*"
-end
+page '/api/*', layout: :api
 
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
@@ -56,12 +37,15 @@ end
 # Methods defined in the helpers block are available in templates
 helpers do
   # Require all functions in the helpers directory
-  require_all "helpers"
+  require_all 'helpers'
 end
 
 # Use Ruby Slim and disable option warning
 require 'slim'
 Slim::Engine.disable_option_validator!
+
+# Activate sprokets + compass
+activate :sprockets
 
 # Import CSS/SCSS
 set :css_dir, 'stylesheets'
@@ -75,8 +59,8 @@ set :images_dir, 'images'
 
 # Activate markdown using Redcarpet parsing engine and syntax highlighting
 set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true, :smartypants => true
-activate :rouge_syntax
+set :markdown, fenced_code_blocks: true, smartypants: true
+activate :syntax
 
 # Pretty index directory structure
 activate :directory_indexes
