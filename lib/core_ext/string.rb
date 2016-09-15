bdbb62c33e44d94d43a163d64bfb63bb654e86d8
Jake Renzella <j@kerenzella.com>
2016-09-15 16:02:59 +1000

    QUALITY: Add Humanized text for backtics

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -7,10 +7,22 @@ module CoreExtensions
       split('_').map(&:capitalize).join(' ')
     end
 
+    def to_kebab_case
+      to_human_case.downcase.tr(' ', '-')
+    end
+
     def to_html
-      renderer = Redcarpet::Render::HTML.new()
-      markdown = Redcarpet::Markdown.new(renderer, extensions = {})
-      markdown.render(self)
+      renderer = Redcarpet::Render::HTML.new
+      markdown = Redcarpet::Markdown.new(renderer, {})
+      markdown.render(humanize_backticks)
+    end
+
+    def humanize_backticks
+      gsub(/`(.*?)`/) do |match|
+        match.gsub(/(?<=`).*(?=`)/) do |inner_match|
+          "`#{inner_match.to_human_case}`"
+        end
+      end
     end
   end
 end
