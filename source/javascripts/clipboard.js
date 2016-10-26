b99c69d0b9d5745dcc31493831c58f7cfd452919
Alex Cummaudo <alexcu@me.com>
2016-10-26 14:26:16 +1100

    ENHANCE: Add popover styling for copying to clipboard

--- source/javascripts/clipboard.js
+++ source/javascripts/clipboard.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
-  new Clipboard('*[data-clipboard]');
+  var sel = '*[data-clipboard]';
+  var els = document.querySelectorAll(sel);
+
+  var clipboard = new Clipboard(sel);
+  clipboard.on('success', function(e) {
+    e.trigger.setAttribute('aria-label', 'Copied to clipboard');
+    $(e.trigger).popover('show');
+  });
+  clipboard.on('failure', function(e) {
+    var actionMsg;
+    if (/iPhone|iPad/i.test(navigator.userAgent)) {
+      actionMsg = 'No support :(';
+    }
+    else if (/Mac/i.test(navigator.userAgent)) {
+      actionMsg = 'Press ⌘-C to copy';
+    }
+    else {
+      actionMsg = 'Press Ctrl-C to copy';
+    }
+    el.setAttribute('aria-label', actionMsg);
+    $(e.trigger).popover('show');
+  });
+
+  for (var i = 0; i < els.length; i++) {
+    var el = els[i];
+    var actionMsg = 'Click to copy';
+    el.setAttribute('aria-label', actionMsg);
+    $(el).popover({
+      trigger: 'hover',
+      placement: 'bottom',
+      content: function() {
+        return el.getAttribute('aria-label');
+      }
+    });
+    el.addEventListener('mouseleave', function(e) {
+      el.setAttribute('aria-label', actionMsg);
+    });
+  }
 })
